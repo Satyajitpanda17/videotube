89d4b62a5eaef3e06874322d9cb83d7499d6eb40
Satyajitpanda17 <satyajitpanda1704@gmail.com>
2024-06-05 12:27:11 +0530

    index file updated

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ( async () => {
+//     try{
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       app.on("error",(error)=>{
+//         console.log("ERRR: ",error);
+//         throw error
+//       })
+
+//      app.listen(process.env.PORT,() => {
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//      })
+
+//     }catch(error){
+//         console.error("ERROR:",error)
+//         throw error
+//     }
+// })()//
