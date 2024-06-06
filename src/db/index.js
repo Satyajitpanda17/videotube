dfe9e4f6d01fa27a63190189dc67dc3e1fe165ac
Satyajitpanda17 <satyajitpanda1704@gmail.com>
2024-06-06 21:52:11 +0530

    file upload code done

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,10 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error",(error)=>{
-                    console.log("ERRR: ",error);
-                    throw error;
-                  })
+        // app.on("error",(error)=>{
+        //             console.log("ERRR: ",error);
+        //             throw error;
+        //           })
         console.log(`\n MONGODB connected !! DB HOST ${connectionInstance.connection.host}`);
         //console.log(`\n ${connectionInstance}`);
     }catch(errror){
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import {v2 as cloudinary} from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,  
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        //upload file on cloudinary
+        cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })//file has been uploaded successfully
+        console.log("file upload on cloudinary",response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)//remove the locally save temp. file as upload file failed
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+    { public_id: "olympic_flag" }, 
+    function(error, result) {console.log(result); });
