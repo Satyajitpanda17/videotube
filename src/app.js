27489cfaf2a29a2ae67d01bdee6590f68648034d
Satyajitpanda17 <satyajitpanda1704@gmail.com>
2024-06-19 20:10:25 +0530

    added like and comment routes

--- src/app.js
+++ src/app.js
@@ -20,9 +20,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 //routes import
-import userRouter from "./routes/user.routes.js";
+import userRouter from "../routes/user.routes.js";
+import commentRouter from "../routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
+app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/likes",likeRouter)
 
 export { app }
